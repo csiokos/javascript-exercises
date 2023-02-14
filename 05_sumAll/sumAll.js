22c8d819bf3b2205903540d8badfedbeb55170b7
csiokos <123376412+csiokos@users.noreply.github.com>
2023-02-14 19:56:11 +0200

    4th and 5th  js exercises done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(num1>num2){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    
+    if (typeof num1 === "number" && typeof num2 === "number" && num1 >= 0 && num2 >= 0){
+        for (i=num1; i<=num2; i++){
+            sum += i;
+
+        }
+
+        return sum;
+
+    } else {
+        return ("ERROR");
+
+    }
+    
 
 };
 
