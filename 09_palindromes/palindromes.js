b1fd4d187d0b29a12267172662a7dc455b41e470
csiokos <123376412+csiokos@users.noreply.github.com>
2023-02-15 11:53:57 +0200

    8th  js exercises done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+  // Check if the string is a palindrome
+  for (let i = 0; i < Math.floor(str.length / 2); i++) {
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
