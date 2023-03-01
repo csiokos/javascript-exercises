a23674f3e6b21227708fdbffbcb38c483ae48587
csiokos <123376412+csiokos@users.noreply.github.com>
2023-03-01 21:47:59 +0200

    find the oldest, used reduce method, and date function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const today = new Date().getFullYear();
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(oldestPerson, today);
+    const currentAge = getAge(currentPerson, today);
+    return currentAge > oldestAge ? currentPerson : oldestPerson;
+  });
+}
 
+function getAge(person, today) {
+  if (person.yearOfDeath) {
+    return person.yearOfDeath - person.yearOfBirth;
+  } else {
+    return today - person.yearOfBirth;
+  }
+    
 };
 
 // Do not edit below this line
