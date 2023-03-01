3562cbe00956da2e40d939f562229957c7da1a84
csiokos <123376412+csiokos@users.noreply.github.com>
2023-03-01 14:43:34 +0200

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // Convert input to a number
+  n = parseInt(n);
 
+  // Check that input is a positive integer
+  if (isNaN(n) || n < 0 || !Number.isInteger(n)) {
+    return "OOPS";
+  }
+
+  // Compute Fibonacci sequence
+  if (n <= 1) {
+    return n;
+  }
+  let prev = 0;
+  let curr = 1;
+  for (let i = 2; i <= n; i++) {
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  return curr;
 };
 
 // Do not edit below this line
